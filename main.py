2412a09dd9dc4202243b2f6f29df4c937b96b113
Neries <6j7W7tRk>
2022-05-18 01:53:40 +0300

    + slider + multiple value check

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 import shutil
 import PySimpleGUI as sg
-import с_dlledit
+from с_dlledit import DllEdit
 
 default_replace_value = '1375'
 
+sg.theme('DarkGreen3')   # Add a touch of color
+
 try:
-    de = с_dlledit.DllEdit()
+    de = DllEdit()
+    cd = de.current_distance
 except Exception as e:
+    print(str(e))
     sg.popup('Error', str(e))
 
-sg.theme('DarkGreen3')   # Add a touch of color
 # All the stuff inside your window.
-layout = [[sg.Text('Current distance:', tooltip=f'Default is {de.default_value}'), sg.Text(de.current_distance, text_color='green', key='current_distance')],
-            [sg.Text('Set new distance:'), sg.InputText(default_replace_value, size=(10, 1), key='new_maxdist')],
-            [sg.Checkbox('Make reserve copy', key='make_reserve_copy')],
-            [sg.Button('Edit'), sg.Button('Exit'), sg.Text('', text_color='green', key='out')]]
+layout = [[sg.Text('Current distance:', tooltip=f'Default is {de.default_value}'), sg.Text(cd, text_color='green', key='current_distance')],
+          #   [sg.Text('Set new distance:'), sg.InputText(default_replace_value, size=(10, 1), key='new_maxdist')],
+          [sg.Text('Set new distance:'), sg.Slider((1000, 2000), cd, orientation='h',
+                                                   key='new_maxdist', font='Courier 8', text_color='green')],
+          [sg.Checkbox('Make reserve copy', key='make_reserve_copy')],
+          [sg.Button('Save'), sg.Button('Exit'), sg.Text('', text_color='green', key='out')]]
 
 # Create the Window
 window = sg.Window('D2Distance', layout, alpha_channel=0.9, icon='app.ico')
@@ -22,34 +27,29 @@ window = sg.Window('D2Distance', layout, alpha_channel=0.9, icon='app.ico')
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks cancel
         break
 
-    if event == 'Edit':
-        new_maxdist = values['new_maxdist']
+    if event == 'Save':
+        new_maxdist = int(values['new_maxdist'])
         out = {'text': '', 'text_color': ''}
 
-        if new_maxdist.isnumeric() and len(new_maxdist) == 4:
-            try:
-                if values['make_reserve_copy']:
-                    shutil.copyfile(dll_path, "client.dll-copy")
+        try:
+            if values['make_reserve_copy']:
+                shutil.copyfile(de.dll_path, "client.dll-copy")
 
-                de.update_distance(new_maxdist)
-                window['current_distance'].update(new_maxdist)
-                out['text'] = '✔️Sucess'
-                out['text_color'] = 'green'
-            except Exception as e:
-                out['text'] = '❌Error'
-                out['text_color'] = 'red'
-                sg.popup(out['text'], str(e))
-        else:
-            out['text'] = '⚠️ Wrong data!'
-            out['text_color'] = 'orange'
-            sg.popup(out['text'], f'New value must be a numeric and contain 4 digits.\nYou entered: {new_maxdist}', keep_on_top=True)
+            de.update_distance(new_maxdist)
+            window['current_distance'].update(new_maxdist)
+            out['text'] = 'Success ✔️'
+            out['text_color'] = 'green'
+        except Exception as e:
+            out['text'] = 'Error ❌'
+            out['text_color'] = 'red'
+            sg.popup(out['text'], str(e))
 
         window['out'].update(out['text'], text_color=out['text_color'])
         window['make_reserve_copy'].update(False)
-        
+
     print('You entered ', values['new_maxdist'])
 
 window.close()
--- с_dlledit.py
+++ с_dlledit.py
@@ -1,13 +1,30 @@
 import winreg
+from functools import cached_property
 
 
 class DllEdit:
     """docstring for DllEdit"""
+
     def __init__(self):
         self.default_value = '1200'
+        f_index = self.dll_data.find('Maximum visible distance')
+        self.posible_indexes = [
+            [f_index-8, f_index-4],
+            [f_index-12, f_index-8],
+        ]
         #self.dll_path = "C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta/game/dota/bin/win64/client.dll"
-    
-    @property
+
+    @cached_property
+    def dll_path(self):
+        aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
+        aKey = winreg.OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall",
+                              0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
+        asubkey = winreg.OpenKey(aKey, 'Steam App 570')
+        #print(winreg.QueryValueEx(asubkey, "DisplayName")[0])
+        # return "client_old.dll"
+        return winreg.QueryValueEx(asubkey, "InstallLocation")[0] + '/game/dota/bin/win64/client.dll'
+
+    @cached_property
     def dll_data(self):
         # Read bytes from .dll and covert them to readable format ansi
         file = open(self.dll_path, "rb")
@@ -15,37 +32,50 @@ class DllEdit:
         data = data.decode("ansi")
         file.close()
         return data
-    
-    @property
-    def dll_path(self):
-        aReg = winreg.ConnectRegistry(None, winreg.HKEY_LOCAL_MACHINE)
-        aKey = winreg.OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", 0, winreg.KEY_READ | winreg.KEY_WOW64_64KEY)
-        asubkey = winreg.OpenKey(aKey, 'Steam App 570')
-        #print(winreg.QueryValueEx(asubkey, "DisplayName")[0])
-        return winreg.QueryValueEx(asubkey, "InstallLocation")[0] + '/game/dota/bin/win64/client.dll'
+
+    @cached_property
+    def indexes(self):
+        result = {
+            'start': 0,
+            'end': 0
+        }
+
+        for variant in self.posible_indexes:
+            pv = self.dll_data[variant[0]:variant[1]]
+            if(pv.isdigit() and len(pv) == 4):
+                result['start'] = variant[0]
+                result['end'] = variant[1]
+
+        return result
+
+    # @cached_property
+    # def f_index(self):
+    #     return self.dll_data.find('Maximum visible distance')
 
     @property
     def current_distance(self):
-        f_index = self.dll_data.find('r_propsmaxdist')
-        return self.dll_data[f_index+16:f_index+20]
+        result = self.dll_data[self.indexes['start']:self.indexes['end']]
+
+        if len(result) != 4:
+            raise Exception("Сan't get current distance :.(")
+
+        return result
 
     def update_distance(self, new_value):
         # Do some stuff with data, as replacement of one string to another.
-        #data = data.replace(search_value, new_value)
-        old_data = self.dll_data
-        f_index = old_data.find('r_propsmaxdist')
-        new_data = old_data[:f_index+16] + new_value + old_data[f_index+20:]
-        #print(data[f_index:f_index+25])
-
+        new_data = self.dll_data[:self.indexes['start']] + \
+            str(new_value) + self.dll_data[self.indexes['end']:]
         # Encode back to bytes content of file
         new_data = new_data.encode("ansi")
         file = open(self.dll_path, "wb")
         file.write(new_data)
         file.close()
 
+
 if __name__ == "__main__":
     de = DllEdit()
     print(f'Current distance: {de.current_distance}')
     new_distance = input('Set new distance: ')
     de.update_distance(new_distance)
-    input(f'Done! New distance is {new_distance}. Press "Enter" to continue...')
+    input(
+        f'Done! New distance is {new_distance}. Press "Enter" to continue...')
